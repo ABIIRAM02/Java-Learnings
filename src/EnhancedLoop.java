a781dd6cab9814b481476669821c450694cc35bf
ABIIRAM02 <abiramofficial002@gmail.com>
2026-01-08 12:14:42 +0530

    Enhanced loops are same as 'for of' in js, however the syntax are different

--- src/EnhancedLoop.java
+++ src/EnhancedLoop.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class EnhancedLoop {
 
     public static void main(String[] arg){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many items you did like to eat ? ");
+        int items = scanner.nextInt();
+        scanner.nextLine();
+
+        String[] foods = new String[items];
+
+        for(int i = 0; i < items; i++){
+            System.out.print("Enter your food: ");
+            foods[i] = scanner.nextLine();
+        }
+
+        for(String value : foods){
+            System.out.println(value);
+        }
+
+        scanner.close();
+    }
+
+    static void enhancedLoop(){
         //Enhanced loop
         String[] fruits = {"apple", "banana", "mango", "grape", "jackfruit"};
 
