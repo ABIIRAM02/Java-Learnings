bdc637d217c1d697606dd45903327df400ba8f25
ABIIRAM02 <abiramofficial002@gmail.com>
2026-01-22 08:59:44 +0530

    Anonymous class example; By this we can able to override from the main class itself.

--- src/Users.java
+++ src/Users.java
@@ -6,6 +6,15 @@ public class Users {
 
         User user2 = new User("Jaan",28,6);
         user2.userBio();
+
+        User user3 = new User("Introvert ram", 24, 4){
+            @Override
+            void userBio(){
+                System.out.println("Gotta go");
+            }
+        };
+
+        user3.userBio();
     }
 
 }
